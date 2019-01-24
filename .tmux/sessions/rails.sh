d1df48a57e62ab1315f13193c0ad7eaa6195b081
Ricardo Villagrana <rvillagrana@protonmail.com>
2019-01-24 13:14:01 -0600

    fix rails.sh on tmux sessions conf

--- .tmux/sessions/rails.sh
+++ .tmux/sessions/rails.sh
@@ -4,7 +4,8 @@ echo 'Setting up Tmux and Vim for you, wait one second...'
 
 tmux new-session -d 'vim'
 sleep 0.5
-tmux split-window -h -p 20 'rails server'
+tmux split-window -h -p 20
+tmux split-window -v -p 40 'rails server'
 tmux split-window -v -p 30 'bin/webpack-dev-server'
 tmux attach-session -d
 
