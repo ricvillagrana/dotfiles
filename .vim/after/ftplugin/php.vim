182716480cf0dd3dfe1f2939392a505f6926f827
Ricardo Villagrana <ricardovillagranal@gmail.com>
2019-10-22 14:45:12 -0500

    add vim dir

---
+++ .vim/after/ftplugin/php.vim
@@ -0,0 +1,3 @@
+et expandtab	    " Use spaces instead of tabs
+set shiftwidth=4    " Number of auto-indent spaces set smartindent
+
--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 ln -s ./.vimrc ~/.vimrc
 ln -s ./.zshrc ~/.zshrc
 ln -s ./.tmux.conf ~/.tmux.conf
+ln -s ./.vim ~/.vim
 
 brew install fzf
 
