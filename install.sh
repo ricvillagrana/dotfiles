13d6f05a5e4a463c76fa4715b962ec9ab25ebe8c
Ricardo Villagrana <ricardovillagranal@gmail.com>
2020-04-13 17:47:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,26 @@
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# Here should be all the necessary commands to install this dotfiles
-ln -s ./.vimrc ~/.vimrc
-ln -s ./.zshrc ~/.zshrc
-ln -s ./.tmux.conf ~/.tmux.conf
-ln -s ./.vim ~/.vim
-
+brew install zsh
 brew install fzf
 brew install ripgrep
 
-vim +PluginInstall +qa
-# Remember to prefix + I on TMUX
-# Remember to add onhalfdark as color iterm color and nord as the colorscheme
+# Install Oh My ZSH
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Download auto-suggestions for ZSH
+(cd $ZSH_CUSTOM/plugins && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions)
 
+# Install plug (plugin manager for vim)
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    
+# Create nvim directory
+mkdir -p ~/.config/nvim
+
+# Here should be all the necessary commands to install this dotfiles
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.tmux ~/.tmux
+
+nvim +PluginInstall +qa
+# Remember to prefix + I on TMUX
